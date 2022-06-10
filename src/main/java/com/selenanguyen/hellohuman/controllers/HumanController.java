761163cd573cca4fdc2ae122eee6bbc42c46be86
Selena Nguyen <missselenah@gmail.com>
2022-06-10 12:38:36 -0700

    senseibonus

--- src/main/java/com/selenanguyen/hellohuman/controllers/HumanController.java
+++ src/main/java/com/selenanguyen/hellohuman/controllers/HumanController.java
@@ -10,8 +10,16 @@ public class HumanController {
 //	 http://127.0.0.1:8080/
 //	 http://127.0.0.1:8080/?name=Shineshine
 	@GetMapping("/")
-	public String hello(@RequestParam(value="name", required =false, defaultValue="Human") String searchQuery) {
-		return "Hello " + searchQuery;
+	public String hello(@RequestParam(value="first_name", required =false, defaultValue="Human") String searchFirst, 
+			@RequestParam(value="last_name", required =false) String searchLast, 
+			@RequestParam(value="count", required =false) Integer counting) {
+		String first = "";
+		String last = "";
+		for(int i=0; i<counting; i++) {
+			first += searchFirst;
+			last += searchLast;
+		}
+		return "You searched for " + first + " " + last + " for " + counting + "times";
 	}
 	
 //	@GetMapping("/")
